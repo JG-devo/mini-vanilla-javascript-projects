c263df0b183e84ec9a807aedc2d4f8b60206bf4f
Jean Gabriel <67287800+JG-devo@users.noreply.github.com>
2022-02-09 16:10:50 +0000

    Add update functions with modal popup

--- 09_expense_tracker/public/script.js
+++ 09_expense_tracker/public/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const balance = document.querySelector('.balance-heading');
+const welcome = document.querySelector('.welcome');
 const income = document.querySelector('.balance__income');
 const expense = document.querySelector('.balance__expense');
 const historyList = document.querySelector('.history__list');
@@ -11,9 +12,15 @@ const amount = document.querySelector('#amount');
 
 const loginContainer = document.querySelector('.login');
 const loginForm = document.querySelector('.login-form');
-const overlay = document.querySelector('.overlay');
 const loginName = document.querySelector('#name');
 const loginPassword = document.querySelector('#password');
+const overlay = document.querySelector('.overlay');
+
+const updateCancel = document.querySelector('.update-form__btn--cancel');
+const updateContainer = document.querySelector('.update');
+const updateForm = document.querySelector('.update-form');
+const updatedAmount = document.querySelector('#update-amount');
+const updatedText = document.querySelector('#update-text');
 
 let transactions = {};
 
@@ -83,6 +90,9 @@ const addTransactionDOM = transaction => {
       <button class="history__list--delete" onclick="removeTransaction('${
         transaction._id
       }')">X</button>
+      <button class="history__list--update" onclick="updateTransaction('${
+        transaction._id
+      }')">U</button>
     </li>`;
 
   historyList.insertAdjacentHTML('beforeend', markup);
@@ -118,13 +128,9 @@ const addTransaction = async e => {
     amount: Number(amount.value),
   };
 
-  console.log(transaction);
-
-  addTransactionDOM(transaction);
-  updateValues();
   try {
     const token = localStorage.getItem('token');
-    await axios.post(
+    const { data } = await axios.post(
       '/api/v1/expenses',
       {
         text: transaction.text,
@@ -136,9 +142,13 @@ const addTransaction = async e => {
         },
       }
     );
+    transactions.push(data.expense);
+    addTransactionDOM(data.expense);
   } catch (error) {
     console.log(error);
   }
+
+  updateValues();
 
   text.value = '';
   amount.value = '';
@@ -146,11 +156,63 @@ const addTransaction = async e => {
 
 const removeTransaction = async id => {
   try {
-    await axios.delete(`/api/v1/expenses/${id}`);
+    const token = localStorage.getItem('token');
+    await axios.delete(`/api/v1/expenses/${id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
     init();
   } catch (error) {
     console.log(error);
   }
+};
+
+const updateTransaction = id => {
+  updateContainer.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+
+  const closeUpdateModal = e => {
+    e.preventDefault();
+    updateContainer.classList.add('hidden');
+    overlay.classList.add('hidden');
+  };
+
+  updateCancel.addEventListener('click', closeUpdateModal);
+  overlay.addEventListener('click', closeUpdateModal);
+
+  const [currentTransaction] = transactions.filter(item => item._id === id);
+  updatedAmount.value = currentTransaction.amount;
+  updatedText.value = currentTransaction.text;
+
+  updateForm.addEventListener('submit', async e => {
+    try {
+      e.preventDefault();
+
+      const token = localStorage.getItem('token');
+
+      if (updatedText.value.trim() === '' || updatedAmount.value.trim() === '')
+        return alert('Please add text and an amount');
+
+      await axios.patch(
+        `/api/v1/expenses/${id}`,
+        {
+          text: updatedText.value,
+          amount: Number(updatedAmount.value),
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      closeUpdateModal(e);
+      loadDatabase(token);
+    } catch (error) {
+      console.log(error);
+    }
+  });
 };
 
 const init = async () => {
